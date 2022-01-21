7812621c4cce033d505c736d344744a6e788d2b5
Hansin1997 <845612500@qq.com>
2022-01-21 18:37:00 +0800

    [UPDATE] gh-pages

--- fun-client-axios/update.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-openapi-generator-cli generate -i http://localhost:8080/doc -g typescript-axios -o .
-tsc --build tsconfig.json
--- fun-kubeless/src/main/java/cn/dustlight/fun/kubeless/entities/kubeless/RuntimeImages.java
+++
@@ -1,7 +0,0 @@
-package cn.dustlight.fun.kubeless.entities.kubeless;
-
-import java.util.LinkedList;
-
-public class RuntimeImages extends LinkedList<RuntimeImage> {
-
-}
